a0fce910e4bce411ca7e7e21833513efc5053199
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 20:16:12 +0300

    Add project files.

---
+++ NewContextMenu/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewContextMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseClick += Form1_MouseClick;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            CustomContextMenu customContextMenu = new CustomContextMenu();
+            customContextMenu.Show(this, new Point(100, 100));
+        }
+    }
+}
